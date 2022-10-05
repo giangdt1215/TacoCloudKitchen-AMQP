4adc7e6c65cb4d9c9669f86de829649a86683765
Dang Giang <gianggiang1512@gmail.com>
2022-10-05 22:21:31 +0700

    Add Queue bean

--- src/main/java/com/dtg/kitchen/messaging/rabbit/RabbitConfig.java
+++ src/main/java/com/dtg/kitchen/messaging/rabbit/RabbitConfig.java
@@ -1,5 +1,7 @@
 package com.dtg.kitchen.messaging.rabbit;
 
+import com.rabbitmq.client.AMQP;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitConfig {
 
+    @Bean
+    public Queue queue(){
+        return new Queue("tacocloud.order.queue", false);
+    }
     @Bean
     public Jackson2JsonMessageConverter messageConverter(){
         return new Jackson2JsonMessageConverter();
